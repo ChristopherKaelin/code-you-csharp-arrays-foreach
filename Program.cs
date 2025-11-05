e7eefc0ee2243123b2526c1ec7d4c02625c28048
Chris 'EagleUK' Kaelin <chriskaelin67@gmail.com>
2025-11-05 05:38:23 -0500

    initial examples

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First fraudulent order ID: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"all fraudulent order IDs: {string.Join(", ", fraudulentOrderIDs)}");
+Console.WriteLine();
